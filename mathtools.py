623af543332857ca3b92eef083613e6fc9f72e88
jonne.taipale <jonne.taipale@gmail.com>
2016-10-07 12:38:59 +0300

    added function

--- mathtools.py
+++ mathtools.py
@@ -19,3 +19,24 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+
+		
+in the desired branch using git status.
+
+
+
+
+
+
+
+
+
+def fib(n):
+    ''' Calculates the n value of the fibonacci sequence'''
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+ 
